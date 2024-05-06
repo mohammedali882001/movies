8f68eb6db32c08320d8a0c91c7a98551d6cc2ade
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-05-06 17:36:51 +0300

    Search is done

--- src/Components/ActorList/ActorList.jsx
+++ src/Components/ActorList/ActorList.jsx
@@ -2,10 +2,28 @@ import React, { useEffect, useState } from "react";
 import styles from "./ActorList.module.css";
 import { axiosInstance } from "../apis/config";
 import ActorCard from "../ActorCard/ActorCard";
+
 export default function ActorList() {
-  const [Actors, setActors] = useState([]);
-  let GetAllActors = () => {
+  const [actors, setActors] = useState([]);
+  const [searchActor, setSearchActor] = useState("");
+  const [originalActors, setOriginalActors] = useState([]);
+
+  const getAllActors = () => {
     axiosInstance.get(`Actor`).then((res) => {
+      if (res.data.isSuccess) {
+        setActors(res.data.data);
+        setOriginalActors(res.data.data);
+      } else {
+        setActors([]);
+        setOriginalActors([]);
+      }
+    });
+  };
+
+  const handleSearchActor = (searchString) => {
+    setSearchActor(searchString);
+    setActors(originalActors);
+    axiosInstance.get(`Actor/${searchString}`).then((res) => {
       if (res.data.isSuccess) {
         setActors(res.data.data);
       } else {
@@ -14,16 +32,46 @@ export default function ActorList() {
     });
   };
 
+  const resetSearch = () => {
+    setSearchActor("");
+    setActors(originalActors);
+  };
+
   useEffect(() => {
-    GetAllActors();
+    getAllActors();
   }, []);
+
+  useEffect(() => {
+    handleSearchActor(searchActor);
+  }, [searchActor]);
 
   return (
     <>
-      <div className=" row  ">
-        {Actors.map((Actor) => (
-          <div key={Actor.id} className=" col col-3  mb-5  ">
-            <ActorCard Actor={Actor}></ActorCard>
+      <div className="container mt-5">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search For Actor"
+            aria-describedby="button-addon2"
+            id="searchActor"
+            value={searchActor}
+            onChange={(e) => handleSearchActor(e.target.value)}
+          />
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+            onClick={resetSearch}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        {actors.map((actor) => (
+          <div key={actor.id} className="col col-3 mb-5">
+            <ActorCard Actor={actor}></ActorCard>
           </div>
         ))}
       </div>
--- src/Components/Directors/Directors.jsx
+++ src/Components/Directors/Directors.jsx
@@ -6,26 +6,70 @@ import DirectorCard from "../DirectorCard/DirectorCard";
 
 export default function Directors() {
   const [directors, setDirectors] = useState([]);
+  const [searchDirector, setSearchDirector] = useState("");
+  const [originalDirectors, setOriginalDirectors] = useState([]);
+
+  let getAllDirectors = () => {
+    axiosInstance.get(`Director`).then((res) => {
+      if (res.data.isSuccess) {
+        setDirectors(res.data.data);
+        setOriginalDirectors(res.data.data);
+      } else {
+        setDirectors([]);
+        setOriginalDirectors([]);
+      }
+    });
+  };
+
+  const handleSearchDirector = (searchString) => {
+    setSearchDirector(searchString);
+    setDirectors(originalDirectors);
+    axiosInstance.get(`Director/${searchString}`).then((res) => {
+      if (res.data.isSuccess) {
+        setDirectors(res.data.data);
+      } else {
+        setDirectors([]);
+      }
+    });
+  };
+
+  const resetSearch = () => {
+    setSearchDirector("");
+    setDirectors(originalDirectors);
+  };
 
   useEffect(() => {
-    const getAllDirectors = async () => {
-      try {
-        const response = await axiosInstance.get(`Director`);
-        if (response.data.isSuccess) {
-          setDirectors(response.data.data);
-        } else {
-          setDirectors([]);
-        }
-      } catch (error) {
-        console.error("Error fetching directors:", error);
-      }
-    };
-
     getAllDirectors();
   }, []);
 
+  useEffect(() => {
+    handleSearchDirector(searchDirector);
+  }, [searchDirector]);
+
   return (
     <>
+      <div className="container mt-5">
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search For Director"
+            aria-describedby="button-addon2"
+            id="searchDirector"
+            value={searchDirector}
+            onChange={(e) => handleSearchDirector(e.target.value)}
+          />
+          <button
+            className="btn btn-outline-secondary"
+            type="button"
+            id="button-addon2"
+            onClick={resetSearch}
+          >
+            Reset
+          </button>
+        </div>
+      </div>
+
       <div className="row">
         {directors.map((director) => (
           <div key={director.id} className="col-md-4 gy-5">
